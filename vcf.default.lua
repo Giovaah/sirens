3fe0b98cbc4f68e7123edd0146e1431e2f79c88a
Giovaah <112887930+Giovaah@users.noreply.github.com>
2023-07-17 17:53:01 +0200

    Add files via upload

---
+++ vcf.default.lua
@@ -0,0 +1,14 @@
+vcf_files = {
+    "POLICE.xml",
+    "POLICE2.xml",
+    "POLICE3.xml",
+    "POLICE4.xml",
+    "PRANGER.xml",
+    "SHERIFF.xml",
+    "SHERIFF2.xml",
+    "AMBULANCE.xml",
+    "FBI2.xml",
+    "FBI.xml",
+    "policeold1.xml",
+    "policeold2.xml"
+}
